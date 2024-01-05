2fa66ab50d47d02f9b1c513e8270a828ef8f1965
Marie-Hélène <mh.roussel@hotmail.fr>
2024-01-05 15:56:37 +0100

    configuration de React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-    return (
-        <div>
-            <h1> Les chats</h1>
-            <p> Sont les maître du monde</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1> Les chats</h1>
+      <p> Sont les maître du monde</p>
+    </div>
+  );
 };
 
 export default App;
